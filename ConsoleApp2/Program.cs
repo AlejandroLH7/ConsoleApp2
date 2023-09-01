298a4daf44a0a3ccf8b718290fd47c8c6d86777d
ESTUDIANTE <ESTUDIANTE@ULELAB312>
2023-09-01 15:34:09 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un caracter: ");
+            string caracter = Console.ReadLine();
+            if (caracter == caracter.ToUpper());
+
+            if (caracter == "A"
+               || caracter == "E"
+               || caracter == "I"
+               || caracter == "O"
+               || caracter == "U")
+            {
+                Console.WriteLine("Es una vocal");
+            }
+            else 
+            {
+                Console.WriteLine("No es una vocal");
+            }
+            Console.ReadLine();
+        }
+    }
+}
